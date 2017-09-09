cbe7587a06f0f0a7667024ed9ca3f4b57550bf53
richarddeng <r.deng212@gmail.com>
2017-09-09 09:11:26 -0400

    nlp patch

--- nlp.py
+++ nlp.py
@@ -1,15 +1,41 @@
-import string
+import json
+import re
 
 #index is the index of the word of the location
 
-def parseLanguage(body, locationFlag):
+def findLocationFlag(convertedJson):
+    loc = ""
+    sal = 0
+    for entity in convertedJson["entities"]:
+        if entity["type"] == "LOCATION":
+            if entity["salience"] > sal:
+                loc = entity["name"]
+                sal = entity["salience"]
+    return loc
+
+def findInList(list, target):
+    print(list)
+    print(target)
+    index = list.index(target[0])
+    length = len(target)
+
+    for t in target:
+        if not t in list[index:index + length]:
+            return -1
+
+    return index
+
+
+def findLocation(body, locationFlag):
+
+    body_split = re.split(r'[;,\s]\s*', body)
 
     words = []
 
-    for word in body.split():
+    for word in body_split:
         words.append(word.strip("#.!"))
 
-    index = words.index(locationFlag)
+    index = findInList(words, locationFlag.split())
 
     if index >= len(body) or index < 0:
         return
@@ -17,11 +43,20 @@ def parseLanguage(body, locationFlag):
         return words[index]
 
     cursor = index
-    while cursor >= 0 and not words[cursor].isdigit():
+    while cursor > 0 and not words[cursor].isdigit():
         cursor -= 1
 
-    return " ".join(words[cursor:index + 1])
+    return " ".join(words[cursor:index + len(locationFlag.split())])
 
-print(parseLanguage("My 83 y.o. Parents in imminent danger at 4922 Loch Lomond #Meyerland. Water knee deep inside home. Mom=heart condition. Dad=Alzheimer's", "Meyerland"))
+convertedJson = dict()
+with open("info.json") as f:
+    convertedJson = json.load(f)
+
+locationFlag = findLocationFlag(convertedJson)
+
+tweet = "Water rising quick please help ASAP please!!\n820 smith street, port Arthur Texas,77640"
+print("tweet: \n\t" + tweet)
+print()
+print("determined location: \n\t" + findLocation(tweet, locationFlag))
 #now words[index] is a number, the 45 in 45 oak avenue mercer county
 
